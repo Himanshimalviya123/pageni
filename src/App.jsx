4ff4f5e8f4b83418cd08a4a8f71c165ff9aab3d7
Himanshimalviya123 <malviyahimanshi14@gmail.com>
2025-06-30 22:37:25 +0530

    update

--- src/App.jsx
+++ src/App.jsx
@@ -19,39 +19,34 @@ const App=()=>{
 
     <Carousel>
       <Carousel.Item>
-<img src={img1}/>     
+<img src={img1} width="100%" height="400px"/>     
    <Carousel.Caption>
-          <h3>First slide label</h3>
           <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
         </Carousel.Caption>
       </Carousel.Item>
       <Carousel.Item>
-<img src={img2}/>     
+<img src={img2} width="100%" height="400px"/>     
         <Carousel.Caption>
-          <h3>Second slide label</h3>
           <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
         </Carousel.Caption>
       </Carousel.Item>
       <Carousel.Item>
-<img src={img3}/>     
+<img src={img3} width="100%" height="400px"/>     
         <Carousel.Caption>
-          <h3>Third slide label</h3>
           <p>
             Praesent commodo cursus magna, vel scelerisque nisl consectetur.
           </p>
         </Carousel.Caption>
       </Carousel.Item>
       <Carousel.Item>
-<img src={img4}/>     
+<img src={img4} width="100%" height="400px"/>     
         <Carousel.Caption>
-          <h3>Second slide label</h3>
           <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
         </Carousel.Caption>
       </Carousel.Item>
       <Carousel.Item>
-<img src={img5}/>     
+<img src={img5} width="100%" height="400px"/>     
         <Carousel.Caption>
-          <h3>Second slide label</h3>
           <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
         </Carousel.Caption>
       </Carousel.Item>
