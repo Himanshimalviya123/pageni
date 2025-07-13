531520a078237795b9a079d8f566a10c045c7db6
Himanshimalviya123 <malviyahimanshi14@gmail.com>
2025-07-13 23:26:04 +0530

    update

--- src/component/footer.jsx
+++ src/component/footer.jsx
@@ -2,6 +2,17 @@ const Footer=()=>{
     return(
         <>
         <p>TITANIC WATCHES</p>
+        <div id="footer">
+            <div><h3>COLLECTION</h3>
+            <p>Titan Automatics<br/>Police Batman<br/>Stellar<br/>Raga Power Pearls<br/>Nebula Jewels<br/>Grandmaster<br/>Maritime</p><br/>
+            </div>
+            <div><h3>CUSTOMER SERVICE</h3>
+            <p>Payment Option<br/>Track order<br/>Encircle program<br/>Find Titan World Stores</p></div>
+            <div><h3>CONTACT US</h3>
+            <p>1800-266-0123<br/>customercare@titan.co.in<br/>Help & Contact<br/>FAQs</p></div>
+            <div><h3>ABOUT TITAN</h3>
+            <p>Brands Protection<br/>Coporate<br/>Careeses<br/>Blog</p></div>
+        </div>
         </>
     )
 }
