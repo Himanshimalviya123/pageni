173aabade70593fd157f52f4021fcce4dc51d5ee
Himanshimalviya123 <malviyahimanshi14@gmail.com>
2025-07-14 22:45:37 +0530

    update

--- src/component/footer.jsx
+++ src/component/footer.jsx
@@ -1,7 +1,11 @@
+import i1 from "../images/i1.webp"
+import i2 from "../images/i2.webp"
 const Footer=()=>{
     return(
         <>
+        
         <p>TITANIC WATCHES</p>
+        <div id="foot">
         <div id="footer">
             <div><h3>COLLECTION</h3>
             <p>Titan Automatics<br/>Police Batman<br/>Stellar<br/>Raga Power Pearls<br/>Nebula Jewels<br/>Grandmaster<br/>Maritime</p><br/>
@@ -12,6 +16,11 @@ const Footer=()=>{
             <p>1800-266-0123<br/>customercare@titan.co.in<br/>Help & Contact<br/>FAQs</p></div>
             <div><h3>ABOUT TITAN</h3>
             <p>Brands Protection<br/>Coporate<br/>Careeses<br/>Blog</p></div>
+        </div>
+        <div><h3>Download Titan World App</h3>
+        <img src={i1}/>
+        <img src={i2}/>
+        <h5>Follow Us With</h5></div>
         </div>
         </>
     )
